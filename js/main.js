0f45d22f1707a70482854e23b46c2e6183af1160
canizalesJd <canizalesjd@gmail.com>
2023-12-08 22:01:58 -0600

    Replacing operators for icons

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const add = (firstNum, secondNum) => {
+  return firstNum + secondNum;
+};
+
+const substract = (firstNum, secondNum) => {
+  return firstNum - secondNum;
+};
+
+const multiply = (firstNum, secondNum) => {
+  return firstNum * secondNum;
+};
+
+const divide = (firstNum, secondNum) => {
+  return firstNum / secondNum;
+};
+
+// Getting number buttons
+const number1 = document.querySelector("#one-btn");
+const number2 = document.querySelector("#two-btn");
+const number3 = document.querySelector("#three-btn");
+const number4 = document.querySelector("#four-btn");
+const number5 = document.querySelector("#five-btn");
+const number6 = document.querySelector("#six-btn");
+const number7 = document.querySelector("#seven-btn");
+const number8 = document.querySelector("#eight-btn");
+const number9 = document.querySelector("#nine-btn");
+const number0 = document.querySelector("#zero-btn");
+
+// Getting operator buttons
+const addBtn = document.querySelector("#add-btn");
+const substractBtn = document.querySelector("#substract-btn");
+const multiplyBtn = document.querySelector("#multiply-btn");
+const divideBtn = document.querySelector("#divide-btn");
+const enterBtn = document.querySelector("#enter-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const backspaceBtn = document.querySelector("#backspace-btn");
