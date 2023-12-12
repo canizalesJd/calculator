93a74add250f5bb8da4b7e87f83ccc8499556dc5
canizalesJd <canizalesjd@gmail.com>
2023-12-11 21:42:56 -0600

    Renaming display updating function

--- js/main.js
+++ js/main.js
@@ -14,8 +14,6 @@ const divide = (firstNum, secondNum) => {
   return parseFloat(firstNum) / parseFloat(secondNum);
 };
 
-const operators = ["+", "-", "*", "/"];
-
 // Getting number buttons
 const number1 = document.querySelector("#one-btn");
 const number2 = document.querySelector("#two-btn");
@@ -42,14 +40,10 @@ const backspaceBtn = document.querySelector("#backspace-btn");
 
 // Creating functions to add numbers to the display
 const display = document.querySelector(".calculator-display");
-const appendToDisplay = (value) => {
+const updateDisplay = (value) => {
   if (display.innerHTML === "0") {
-    display.innerHTML = "";
-  }
-  if (value === "." && !display.innerHTML.includes(".")) {
-    display.innerHTML += value;
-  }
-  if (value !== ".") {
+    display.innerHTML = value;
+  } else {
     display.innerHTML += value;
   }
 };
@@ -70,15 +64,29 @@ const backspace = () => {
 backspaceBtn.addEventListener("click", backspace);
 
 // Adding number values to display
-number0.addEventListener("click", appendToDisplay.bind(this, 0));
-number1.addEventListener("click", appendToDisplay.bind(this, 1));
-number2.addEventListener("click", appendToDisplay.bind(this, 2));
-number3.addEventListener("click", appendToDisplay.bind(this, 3));
-number4.addEventListener("click", appendToDisplay.bind(this, 4));
-number5.addEventListener("click", appendToDisplay.bind(this, 5));
-number6.addEventListener("click", appendToDisplay.bind(this, 6));
-number7.addEventListener("click", appendToDisplay.bind(this, 7));
-number8.addEventListener("click", appendToDisplay.bind(this, 8));
-number9.addEventListener("click", appendToDisplay.bind(this, 9));
+number0.addEventListener("click", updateDisplay.bind(this, 0));
+number1.addEventListener("click", updateDisplay.bind(this, 1));
+number2.addEventListener("click", updateDisplay.bind(this, 2));
+number3.addEventListener("click", updateDisplay.bind(this, 3));
+number4.addEventListener("click", updateDisplay.bind(this, 4));
+number5.addEventListener("click", updateDisplay.bind(this, 5));
+number6.addEventListener("click", updateDisplay.bind(this, 6));
+number7.addEventListener("click", updateDisplay.bind(this, 7));
+number8.addEventListener("click", updateDisplay.bind(this, 8));
+number9.addEventListener("click", updateDisplay.bind(this, 9));
 // Adding decimal value to display
-decimal.addEventListener("click", appendToDisplay.bind(this, "."));
+decimal.addEventListener("click", updateDisplay.bind(this, "."));
+
+// Operator functions
+addBtn.addEventListener("click", () => {
+  console.log("+");
+});
+substractBtn.addEventListener("click", () => {
+  console.log("-");
+});
+multiplyBtn.addEventListener("click", () => {
+  console.log("*");
+});
+divideBtn.addEventListener("click", () => {
+  console.log("/");
+});
